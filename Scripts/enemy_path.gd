cfb384f6f9aa50db47a9d77a9e881c786b96945b
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-12-20 16:46:12 -0500

    create enemy using code

--- Scripts/enemy_path.gd
+++ Scripts/enemy_path.gd
@@ -1,7 +1,6 @@
-extends PathFollow2D
-class_name enemy
+extends Path2D
 
-@export var speed = 2
+var enemy = preload("res://Scenes/enemy.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +8,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress+=speed
+	pass
+
+
+func _on_main_createenemysig() -> void:
+	var enemyguy = enemy.instantiate()
+	add_child(enemyguy)
